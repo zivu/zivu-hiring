a70ae138fda5695fae3dcbeffe0e3e53b3e3ebd5
pavlo_zvarych <rj.pavlo.zvarych@gmail.com>
2022-01-25 22:10:14 +0100

    zivu-hiring spring boot base app

---
+++ src/main/java/com/zivu/hiring/ZivuHiringApplication.java
@@ -0,0 +1,13 @@
+package com.zivu.hiring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZivuHiringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZivuHiringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zivu/hiring/ZivuHiringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zivu.hiring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZivuHiringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
