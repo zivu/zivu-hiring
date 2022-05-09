e142b4cbfa4a6fe6d64f73f5cb688cb4d5e529ab
pavlo_zvarych <rj.pavlo.zvarych@gmail.com>
2022-05-09 22:39:09 +0200

    add jpql to further modify query to fetch 5 last questions from every of specified technology

--- src/main/java/com/zivu/hiring/persistance/QuestionRepository.java
+++ src/main/java/com/zivu/hiring/persistance/QuestionRepository.java
@@ -3,6 +3,8 @@ package com.zivu.hiring.persistance;
 import com.zivu.hiring.model.Level;
 import com.zivu.hiring.model.Technology;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Collection;
@@ -14,6 +16,7 @@ import java.util.List;
 @Repository
 public interface QuestionRepository extends JpaRepository<Question, Long> {
 
-    List<Question> findByLevelAndTechnologyIn(Level level, Collection<Technology> technologies);
+    @Query("SELECT q FROM Question q WHERE q.level = :level AND q.technology IN :technologies")
+    List<Question> findByLevelAndTechnologyIn(@Param("level") Level level, @Param("technologies") Collection<Technology> technologies);
     
 }
