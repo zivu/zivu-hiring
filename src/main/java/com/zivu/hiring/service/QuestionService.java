91975ea849b78271c6a440bdf253f44b67e2d2a7
pavlo_zvarych <rj.pavlo.zvarych@gmail.com>
2022-05-11 21:36:55 +0200

    Revert "custom repo impl"
    This reverts commit 32b43daca16e56f427160a93b40589362d1a39e7.

--- src/main/java/com/zivu/hiring/service/QuestionService.java
+++ src/main/java/com/zivu/hiring/service/QuestionService.java
@@ -13,9 +13,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 /**
  * Queries DB for interview questions with specified level of complexity (JUNIOR, MIDDLE, SENIOR)
@@ -43,9 +41,9 @@ public class QuestionService {
      */
     public List<QuestionData> findQuestions(@NonNull Level level, int number, boolean hasJava, boolean hasSpring, boolean hasSql, boolean hasJavaScript) {
         throwExceptionIfNoTechnologyChosen(hasJava, hasSpring, hasSql, hasJavaScript);
-        Set<Technology> technologies = collectRequestedTechnologies(hasJava, hasSpring, hasSql, hasJavaScript);
+        List<Technology> technologies = collectRequestedTechnologies(hasJava, hasSpring, hasSql, hasJavaScript);
         log.info("Retrieving interview questions from DB");
-        List<Question> questions = repository.findFiveNewQuestionsForLevelAndEveryTechnology(level, technologies);
+        List<Question> questions = repository.findByLevelAndTechnologyIn(level, technologies);
         log.info("Successfully retrieved questions from DB");
         return convertToQuestionData(questions);
     }
@@ -77,9 +75,9 @@ public class QuestionService {
         return questionData;
     }
 
-    private Set<Technology> collectRequestedTechnologies(boolean hasJava, boolean hasSpring, boolean hasSql, boolean hasJavaScript) {
+    private List<Technology> collectRequestedTechnologies(boolean hasJava, boolean hasSpring, boolean hasSql, boolean hasJavaScript) {
         log.info("Preparing a list of technologies for DB request");
-        Set<Technology> technologies = new HashSet<>(4);
+        List<Technology> technologies = new ArrayList<>(4);
         if (hasJava) {
             technologies.add(Technology.JAVA);
         }
