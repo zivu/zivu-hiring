e9ab0248c1b4a73d40c82bf3188cbc8f2c989a03
pavlo_zvarych <rj.pavlo.zvarych@gmail.com>
2022-09-03 09:44:47 +0200

    cors added

--- src/main/java/com/zivu/hiring/resource/InterviewQuestionController.java
+++ src/main/java/com/zivu/hiring/resource/InterviewQuestionController.java
@@ -6,6 +6,7 @@ import com.zivu.hiring.service.QuestionService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -21,6 +22,7 @@ import java.util.List;
  * or
  * <a href="http://localhost8080/questions/all">request all questions</a>
  */
+@CrossOrigin
 @Slf4j
 @RestController
 @RequestMapping("/questions")
